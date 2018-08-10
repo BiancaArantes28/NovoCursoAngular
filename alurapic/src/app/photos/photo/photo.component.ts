4ebacfcba8819f36e4140ffec2317d06f4156703
BiancaArantes28 <biancaarantes28@gmail.com>
2018-08-10 16:43:01 -0300

    Colocando os plugins no angular e fazendo o curso até o capítulo 2

---
+++ alurapic/src/app/photos/photo/photo.component.ts
@@ -0,0 +1,10 @@
+﻿import { Component, Input } from "@angular/core";
+
+@Component({
+    selector: 'ap-photo',
+    templateUrl: './photo.component.html'
+})
+export class PhotoComponent {
+    @Input() url = '';
+    @Input() description = '';
+}
